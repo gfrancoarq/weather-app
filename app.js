86a85bf72c12e69a2746a55c300b1a86b8eab07e
Gustavo Franco <53024379+gfrancoarq@users.noreply.github.com>
2022-10-18 14:35:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const API_KEY = 'd74125b8c739a0641b233c5ed82e786f';
 
 const fetchData = position => {
     const { latitude, longitude } = position.coords;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
         .then(response => response.json())
         .then(data => setWeatherData(data))
 };
